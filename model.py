aed35a5731af18fc5655c5396fe01046f6513c29
Mitch Henline <rmhenline@gmail.com>
2023-01-03 13:39:24 -0700

    server and db setup

--- model.py
+++ model.py
@@ -114,3 +114,16 @@ class Calendar(db.Model):
 
     sales_adv = db.relationship("SalesAdv", backref="calendar")
 
+def connect_to_db(flask_app, db_uri=os.environ["POSTGRES_URI"], echo=False):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Database connected.")
+
+if __name__ == "__main__":
+    from server import app
+    connect_to_db(app)
