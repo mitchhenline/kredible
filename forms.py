5bc80d9a2855bdb6cefb23911b1542e9b07aaa39
Mitch Henline <rmhenline@gmail.com>
2023-03-14 23:31:09 -0600

    started request mtg by adv page

--- forms.py
+++ forms.py
@@ -34,16 +34,18 @@ def MeetingFunc(rep_id):
         cust_id=SelectField('Customer ', choices=choices)
     return RequestMeeting()
 
-# def MeetingFuncCust(rep_id):
-#     class RequestMeetingCust(FlaskForm):
+# def MeetingFuncByAdv(rep_id):
+#     time_choices = [("8:00 AM", "8:00 AM"), ("8:15 AM", "8:15 AM"), ("8:30 AM", "8:30 AM"), ("8:45 AM", "8:45 AM"), ("9:00 AM", "9:00 AM"), ("9:15 AM", "9:15 AM"), ("9:30 AM", "9:30 AM"), ("9:45 AM", "9:45 AM"), ("10:00 AM", "10:00 AM"), ("10:15 AM", "10:15 AM"), ("10:30 AM", "10:30 AM"), ("10:45 AM", "10:45 AM"), ("11:00 AM", "11:00 AM"), ("11:15 AM", "11:15 AM"), ("11:30 AM", "11:30 AM"), ("11:45 AM", "11:45 AM"), ("12:00 PM", "12:00 PM"), ("12:15 PM", "12:15 PM"), ("12:30 PM", "12:30 PM"), ("12:45 PM", "12:45 PM"), ("1:00 PM", "1:00 PM"), ("1:15 PM", "1:15 PM"),("1:30 PM", "1:30 PM"), ("1:45 PM", "1:45 PM"), ("2:00 PM", "2:00 PM"), ("2:15 PM", "2:15 PM"),("2:30 PM", "2:30 PM"), ("2:45 PM", "2:45 PM"), ("3:00 PM", "3:00 PM"), ("3:15 PM", "3:15 PM"),("3:30 PM", "3:30 PM"), ("3:45 PM", "3:45 PM"), ("4:00 PM", "4:00 PM"), ("4:15 PM", "4:15 PM"),("4:30 PM", "4:30 PM"), ("4:45 PM", "4:45 PM"), ("5:00 PM", "5:00 PM"), ("5:15 PM", "5:15 PM"),("5:30 PM", "5:30 PM"), ("5:45 PM", "5:45 PM"), ("6:00 PM", "6:00 PM")]
+#     class RequestMeetingByAdv(FlaskForm):
 #         date=DateField('Date ')
-#         time=StringField('Time ')
-#         # time=TimeField('Time ', format='%I:%M:%p') format I had that isn't working
-#         meeting_link=StringField('Meeting link ', [validators.InputRequired()])
+#         time=SelectField('Time ', choices = time_choices)
+#         meeting_link=StringField('Meeting Link ', [validators.InputRequired()])
 #         meeting_prep_notes=TextAreaField('Meeting Notes ')
-#         choices = [ (c.adv_id, c.first_name +" "+ c.last_name) for c in SalesAdv.query.filter_by(sales_rep['rep_id'] == rep_id).all()]
-#         adv_id=SelectField('Advocate ', choices=choices)
-#     return RequestMeetingCust()           ######choices line is holding this function back####
+#         choices = [ (a.adv_id, a.first_name +" "+ a.last_name) for a in SalesAdv.query.filter_by(rep_id = rep_id).all()]
+#         adv_id=SelectField('Sales Advocate ', choices=choices)
+#     return RequestMeetingByAdv
+
+# commented out section not tested yet. work on this next. Add form to server page etc
 
 class AcceptMeeting(FlaskForm):
     meeting_accepted=SubmitField('Accept')
